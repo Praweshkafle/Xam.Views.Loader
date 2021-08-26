ed6efbcd20371859ab7bf19f9c199464fdcaa07d
Praweshkafle <praweshkafle12@gmail.com>
2021-08-26 12:57:39 +0545

    Enums created

---
+++ Xam.Views.Loader/Xam.Views.Loader.Portable/Enums/SpeedDuration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xam.Views.Loader.Portable.Enums
+{
+    public enum SpeedDuration
+    {
+        slow = 7000,
+        medium = 5000,
+        fast = 3000
+    }
+}

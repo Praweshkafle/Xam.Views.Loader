5f2fbea897057d8be8b34c45d1988ddbca265b51
Praweshkafle <praweshkafle12@gmail.com>
2021-08-27 16:33:32 +0545

    CircularLoader added

---
+++ Xam.Views.Loader/Xam.Views.Loader.Portable/CustomControls/CircularLoader.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+using Xam.Views.Loader.Portable.Enums;
+using Xamarin.Forms;
+
+namespace Xam.Views.Loader.Portable.CustomControls
+{
+    public class CircularLoader: ContentView,INotifyPropertyChanged
+    {
+
+        public static readonly BindableProperty LoadingTypeProperty =
+            BindableProperty.Create(nameof(LoadingType), typeof(LoadingTypes),
+                typeof(LinearLoader), defaultValue: LoadingTypes.circle, propertyChanged: DirectionChanged);
+        
+        public static readonly BindableProperty IsLoadingProperty =
+           BindableProperty.Create(nameof(IsLoading), typeof(bool), typeof(LinearLoader), 
+               defaultValue: false, propertyChanged: IsLoadingChanged);
+
+        public LoadingTypes LoadingType
+        {
+            get => (LoadingTypes)GetValue(LoadingTypeProperty);
+            set => SetValue(LoadingTypeProperty, value);
+        }
+        public bool IsLoading
+        {
+            get => (bool)GetValue(IsLoadingProperty);
+            set => SetValue(IsLoadingProperty, value);
+        }
+        private static void DirectionChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            //
+        }
+        private static async void IsLoadingChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if ((bool)newValue)
+            {
+               await ((CircularLoader)bindable).AnimatingView();
+            }
+            else
+                ((CircularLoader)bindable).StopAnimation();
+
+        }
+
+        public Animation rotation;
+
+        public CircularLoader()
+        {
+           
+        }
+
+        private async Task AnimatingView()
+        {
+            Action<double> degree = a => this.Rotation = a;
+            if (LoadingType==LoadingTypes.circle)
+            {
+                while (IsLoading)
+                {
+                    this.Animate(name: "circle", callback: degree, start: 0, end: 360, length: 1000, easing: Easing.Linear);
+                    await Task.Delay(1000);
+                    //rotation.Commit(this, "circleRotate", 16, 1000, Easing.SinIn, (a, b) => { this.RotationX = 0; }, () => true);
+                }
+            }
+            else if (LoadingType == LoadingTypes.flip)
+            {
+
+            }
+        }
+        void StopAnimation()
+        {
+            this.CancelAnimations();
+        }
+    }
+}

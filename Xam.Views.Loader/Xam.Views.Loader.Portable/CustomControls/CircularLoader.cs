db1e6a6911a7178f8ccc1b2bc08f2811fc4047fd
Praweshkafle <praweshkafle12@gmail.com>
2021-08-27 22:22:14 +0545

    bug fixex

--- Xam.Views.Loader/Xam.Views.Loader.Portable/CustomControls/CircularLoader.cs
+++ Xam.Views.Loader/Xam.Views.Loader.Portable/CustomControls/CircularLoader.cs
@@ -14,7 +14,11 @@ namespace Xam.Views.Loader.Portable.CustomControls
         public static readonly BindableProperty LoadingTypeProperty =
             BindableProperty.Create(nameof(LoadingType), typeof(LoadingTypes),
                 typeof(LinearLoader), defaultValue: LoadingTypes.circle, propertyChanged: DirectionChanged);
-        
+
+        public static readonly BindableProperty LoadingSpeedProperty =
+                   BindableProperty.Create(nameof(LoadingType), typeof(LoadingSpeed),
+                       typeof(LinearLoader), defaultValue: LoadingSpeed.fast, propertyChanged: SpeedChanged);
+
         public static readonly BindableProperty IsLoadingProperty =
            BindableProperty.Create(nameof(IsLoading), typeof(bool), typeof(LinearLoader), 
                defaultValue: false, propertyChanged: IsLoadingChanged);
@@ -24,14 +28,24 @@ namespace Xam.Views.Loader.Portable.CustomControls
             get => (LoadingTypes)GetValue(LoadingTypeProperty);
             set => SetValue(LoadingTypeProperty, value);
         }
+        public LoadingSpeed Speed
+        {
+            get => (LoadingSpeed)GetValue(LoadingSpeedProperty);
+            set => SetValue(LoadingSpeedProperty, value);
+        }
         public bool IsLoading
         {
             get => (bool)GetValue(IsLoadingProperty);
             set => SetValue(IsLoadingProperty, value);
         }
-        private static void DirectionChanged(BindableObject bindable, object oldValue, object newValue)
+        private static void SpeedChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+
+        }
+        private static async void DirectionChanged(BindableObject bindable, object oldValue, object newValue)
         {
             //
+            await ((CircularLoader)bindable).AnimatingView();
         }
         private static async void IsLoadingChanged(BindableObject bindable, object oldValue, object newValue)
         {
@@ -56,8 +70,8 @@ namespace Xam.Views.Loader.Portable.CustomControls
             {
                 while (IsLoading)
                 {
-                    this.Animate(name: "circle", callback: degree, start: 0, end: 360, length: 1000, easing: Easing.Linear);
-                    await Task.Delay(1000);
+                    this.Animate(name: "circle", callback: degree, start: 0, end: 360, length: (uint)Speed, easing: Easing.Linear);
+                    await Task.Delay((int)Speed);
                     //rotation.Commit(this, "circleRotate", 16, 1000, Easing.SinIn, (a, b) => { this.RotationX = 0; }, () => true);
                 }
             }
@@ -66,9 +80,9 @@ namespace Xam.Views.Loader.Portable.CustomControls
                 while (IsLoading)
                 {
                     // this.Animate(name: "circle", callback: degree, start: 0, end: 360, length: 1000, easing: Easing.Linear);
-                    await this.RotateYTo(-90, 300, Easing.SinIn);
+                    await this.RotateYTo(-90, (uint)Speed, Easing.SinIn);
                     this.RotationY = 90;
-                    await this.RotateYTo(0, 300, Easing.SinIn);
+                    await this.RotateYTo(0, (uint)Speed, Easing.SinIn);
                 }
             }
         }
@@ -77,5 +91,12 @@ namespace Xam.Views.Loader.Portable.CustomControls
         {
             this.CancelAnimations();
         }
+    }
+
+    public enum LoadingSpeed
+    {
+        slow=3000,
+        medium=2000,
+        fast=800
     }
 }
